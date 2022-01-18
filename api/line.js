10d20a2066c47f3682539de7e4c75038801203bc
alexWangfg <alexwang@futuregame.com.tw>
2022-01-18 22:02:20 +0800

    [modify]刪除comsole

--- api/line.js
+++ api/line.js
@@ -12,15 +12,13 @@ export function setUserInfo(code) {
     data: {
       grant_type: 'authorization_code',
       code: code,
-      redirect_uri: 'http://192.168.2.174:8081',
+      redirect_uri: 'http://192.168.0.227:8081',
       client_id: '1656806827',
       client_secret: '3648e376686ce8b7f94c4ab611515233'
     },
     success: function (res) {
-      console.log(res)
       const user_id = jwtDecode(res.id_token).sub
       sessionStorage.setItem('user_id', user_id)
-      console.log(sessionStorage.getItem('user_id'))
     }
   })
 }
